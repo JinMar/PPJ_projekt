74e36478d66e20554f50ffddcc1f137fc8cb6c28
Marek <Jindrak.marek@gmail.com>
2016-05-31 11:24:47 +0200

    Doplnění restAPi

--- src/main/java/cz/tul/controllers/BasicController.java
+++ src/main/java/cz/tul/controllers/BasicController.java
@@ -15,8 +15,10 @@ import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -26,7 +28,7 @@ import java.util.List;
 /**
  * Created by Marek on 28.05.2016.
  */
-@RestController
+@Controller
 
 @RequestMapping("/")
 public class BasicController {
@@ -333,183 +335,6 @@ public class BasicController {
         logger.info("Comment saved");
 
     }return view;}
-    @RequestMapping(value = "/saveAutor", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    List<Box> saveAutors(@RequestBody final List<Box> box) {
-        List<Box> boxes = new ArrayList<>();
 
-        for (Box item : box) {
-            if (item.getName().length() > 0) {Box box1 =new Box();
-                Autor insert = new Autor(""+ObjectId.get(),item.getName(), new Date(System.currentTimeMillis()));
-                baseAutorRepository.save(insert);
-                box1.setName(insert.getName());
-                box1.setAutorId(insert.getId());
-                System.out.println("Autor saved: " + item.getName());
-                boxes.add(box1);
-            }
-
-        }
-        logger.info("saveAutor done !");
-        return boxes;
-    }
-    @RequestMapping(value = "/saveImage", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    List<Box> saveImages(@RequestBody final List<Box> box) {
-        List<Box> boxes = new ArrayList<>();
-
-        for (Box item : box) {
-            if (item.getName().length() > 0) {
-                Autor autor = baseAutorRepository.findOne(item.getAutorId());
-                if (autor != null) {
-                    Box box1 =new Box();
-                    box1.setName(item.getName());
-                    box1.setAutorId(autor.getId());
-                    int max = baseImagesRepository.findFirstByPositionGreaterThanOrderByPositionDesc(0).getPosition()+1;
-                    Images insert = new Images( ""+ObjectId.get(),item.getUrl(),item.getName(), new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()), autor,max);
-                    baseImagesRepository.save(insert);
-                    box1.setImageId(insert.getId());
-                    System.out.println("Image saved: " + item.getName());
-                    boxes.add(box1);
-                }
-            }
-
-        }
-
-        logger.info("saveImage done !");
-        return boxes;
-    }
-
-
-
-
-
-
-
-    @RequestMapping(value = "/saveComment", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    List<Box> saveComment(@RequestBody final List<Box> box) {
-        List<Box> boxes = new ArrayList<>();
-        logger.info("Start saving comment");
-        for (Box item : box) {
-            Images images = baseImagesRepository.findOne(item.getImageId());
-            Autor autor = baseAutorRepository.findOne(item.getAutorId());
-            if (images != null && autor!=null) {
-
-                Box box1 =new Box();
-                Comment insert = new Comment(""+ObjectId.get(),item.getDescription(), new Date(System.currentTimeMillis()),images,autor);
-                baseCommentRepository.save(insert);
-                box1.setImageId(images.getId());
-                box1.setAutorId(autor.getId());
-                box1.setCommentId(insert.getId());
-                boxes.add(box1);
-                logger.info("AutorId: "+autor.getId()+ " imageId: " +images.getId() +" text: "+insert.getDescription());
-            }
-
-            logger.info("saveComment done !");
-    }
-        return boxes;
-}
-    @RequestMapping(value = "/saveCommentLike", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    List<Box> saveCommentLike(@RequestBody final List<Box> box) {
-        List<Box> boxes = new ArrayList<>();
-
-
-        logger.info("saveCommentLike done !");
-        return boxes;
-    }
-    @RequestMapping(value = "/img/{name}", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    List<Box> imgByName(@PathVariable("name") String name) {
-        List<Box> boxes = new ArrayList<>();
-
-        for(Images item : baseImagesRepository.findByName(name)){
-            Box box =new Box();
-            box.setAutorId(item.getAutor().getId());
-            box.setImageId(item.getId());
-            box.setName(item.getName());
-            box.setUrl(item.getUrl());
-            boxes.add(box);
-
-        }
-        logger.info("img done !");
-        return boxes;
-    }
-    @RequestMapping(value = "/imgt/{id}", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    List<Box> imgBytag(@PathVariable("id") String id) {
-        List<Box> boxes = new ArrayList<>();
-        System.out.println(baseImagesRepository.count());
-        Images image = baseImagesRepository.findByTagsesLike(id);
-
-            Box box =new Box();
-            box.setAutorId(image.getAutor().getId());
-            box.setImageId(image.getId());
-            box.setName(image.getTagses());
-            box.setUrl(image.getUrl());
-
-            boxes.add(box);
-
-
-        logger.info("imgt done !");
-        return boxes;
-    }
-    @RequestMapping(value = "/imga/{id}", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    List<Box> imgByAutor(@PathVariable("id") String id) {
-
-        List<Box> boxes = new ArrayList<>();
-        Autor autor = baseAutorRepository.findOne(id);
-        for(Images item : baseImagesRepository.findByAutor(autor)){
-            Box box =new Box();
-            box.setAutorId(item.getAutor().getId());
-            box.setImageId(item.getId());
-            box.setName(item.getName());
-            box.setUrl(item.getUrl());
-            boxes.add(box);
-
-        }
-        logger.info("imga done !");
-        return boxes;
-    }
-
-    @RequestMapping(value = "/updateimgname", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    List<Box> updateName(@RequestBody final List<Box> box) {
-
-        List<Box> boxes = new ArrayList<>();
-        Images item = baseImagesRepository.findOne(box.get(0).getImageId());
-        if(item !=null) {
-            item.setName(box.get(0).getName());
-            baseImagesRepository.save(item);
-            Box box1 = new Box();
-            box1.setAutorId(item.getAutor().getId());
-            box1.setImageId(item.getId());
-            box1.setName(item.getName());
-            box1.setUrl(item.getUrl());
-            boxes.add(box1);
-        }
-
-        logger.info("update done !");
-        return boxes;
-    }
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public
-    @ResponseBody
-    List<Box> deleteImage(@PathVariable("id") String id) {
-
-        List<Box> boxes = new ArrayList<>();
-        baseImagesRepository.delete(baseImagesRepository.findOne(id));
-        logger.info("delete done !");
-        return boxes;
-    }
 
 }
